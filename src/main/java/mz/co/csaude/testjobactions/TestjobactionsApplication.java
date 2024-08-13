51f74cc10e6b000097c15ab2375d8ef91ab23dfd
guimino <minoneves@gmail.com>
2024-08-13 13:01:00 +0200

    first commit

---
+++ src/main/java/mz/co/csaude/testjobactions/TestjobactionsApplication.java
@@ -0,0 +1,13 @@
+package mz.co.csaude.testjobactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjobactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestjobactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mz/co/csaude/testjobactions/TestjobactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.csaude.testjobactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestjobactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
